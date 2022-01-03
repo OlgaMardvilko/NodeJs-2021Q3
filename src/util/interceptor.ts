a7279550f5995423b0706005e809533c213b4c57
Olga Mardvilko <omardvilko@mail.ru>
2022-01-04 00:55:32 +0300

    feat: #HW-7 Unit tests and config

---
+++ src/util/interceptor.ts
@@ -0,0 +1,19 @@
+export const interceptor = {
+  mockRequest: () => {
+    const req: any = {};
+    req.body = jest.fn().mockReturnValue(req);
+    req.params = jest.fn().mockReturnValue(req);
+    req.query = jest.fn().mockReturnValue(req);
+    return req;
+  },
+
+  mockResponse: () => {
+    const res: any = {};
+    res.send = jest.fn().mockReturnValue(res);
+    res.status = jest.fn().mockReturnValue(res);
+    res.json = jest.fn().mockReturnValue(res);
+    return res;
+  },
+
+  mockNext: () => jest.fn()
+};
